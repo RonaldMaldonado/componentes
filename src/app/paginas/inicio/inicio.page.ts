af85991f922cb80080e30d6d165f95a914d66549
Ronald Maldonado <ronald.maldonado@gmail.com>
2021-01-20 09:23:32 -0400

    ActionSheet

--- src/app/paginas/inicio/inicio.page.ts
+++ src/app/paginas/inicio/inicio.page.ts
@@ -1,12 +1,45 @@
 import { Component, OnInit } from '@angular/core';
 
+interface Componente {
+  icono: string;
+  nombre:string;
+  direccion:string
+}
+
 @Component({
   selector: 'app-inicio',
   templateUrl: './inicio.page.html',
   styleUrls: ['./inicio.page.scss'],
 })
 export class InicioPage implements OnInit {
-
+  componente:Componente[]=[
+    {
+    icono:'person',
+    nombre:'Action Sheet',
+    direccion:'/action-sheet'
+    },
+    {
+      icono:'pricetag',
+      nombre:'Alert',
+      direccion:'/alert'
+    },
+    {
+      icono:'planet',
+      nombre:'Fab',
+      direccion:'/fab'
+    },
+    {
+      icono:'rocket',
+      nombre:'Date Time',
+      direccion:'/datetime'
+    },
+    {
+      icono:'albums',
+      nombre:'Botones',
+      direccion:'/buttons'
+    },
+  ]  
+ 
   constructor() { }
 
   ngOnInit() {
